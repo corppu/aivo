9b4c0f917dd742e8039a0162194ab62ee6d92a62
corppu <corppu91@gmail.com>
2015-12-22 15:32:38 +0200

    Implementing test cases... still unfinished

--- app/src/androidTest/java/com/aivo/hyperion/aivo/ApplicationTest.java
+++ app/src/androidTest/java/com/aivo/hyperion/aivo/ApplicationTest.java
@@ -3,6 +3,9 @@ package com.aivo.hyperion.aivo;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.aivo.hyperion.aivo.models.Mindmap;
+import com.aivo.hyperion.aivo.models.ModelListener;
+import com.aivo.hyperion.aivo.models.ModelMediator;
 import com.aivo.hyperion.aivo.models.User;
 
 /**
@@ -13,9 +16,23 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
+    private MockListener listener1;
+    private MockListener listener2;
+
     @Override
     public void setUp() throws Exception {
         super.setUp();
+
+        listener1 = new MockListener();
+        listener2 = new MockListener();
+
+
+        ModelMediator mediator = new ModelMediator();
+        listener1.setMediator(mediator);
+        listener2.setMediator(mediator);
+
+        mediator.registerListener(listener1);
+        mediator.registerListener(listener2);
     }
 
     @Override
@@ -25,12 +42,17 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
 
 
 
-    public void testName() throws Exception {
-
+    public void testCreateUser() throws Exception {
+        listener1.getMediator().openUser(-1);
+        wait(1000);
+        assertEquals(true, listener1.getUser() != null);
+        assertEquals(true, listener2.getUser() != null);
+        assertEquals(true, listener1.getUser().getId() == listener2.getUser().getId());
     }
 
 
-    public void testCreateUser() {
+    public void testCreateMindmap() throws  Exception {
+        listener1.getMediator().openMindmap(-1);
 
     }
 }
---
+++ app/src/androidTest/java/com/aivo/hyperion/aivo/MockListener.java
@@ -0,0 +1,93 @@
+package com.aivo.hyperion.aivo;
+
+import com.aivo.hyperion.aivo.models.Mindmap;
+import com.aivo.hyperion.aivo.models.ModelListener;
+import com.aivo.hyperion.aivo.models.ModelMediator;
+import com.aivo.hyperion.aivo.models.User;
+
+/**
+ * Created by corpp on 22.12.2015.
+ */
+
+public class MockListener implements ModelListener {
+
+
+
+    private Exception exception;
+    private ModelMediator mediator;
+    private User user;
+    private Mindmap mindmap;
+
+    public MockListener() {
+        this.mediator = null;
+        this.user = null;
+        this.mindmap = null;
+    }
+
+    public Exception getException() {
+        return exception;
+    }
+
+    public void setException(Exception exception) {
+        this.exception = exception;
+    }
+
+    public ModelMediator getMediator() {
+        return mediator;
+    }
+
+    public void setMediator(ModelMediator mediator) {
+        this.mediator = mediator;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Mindmap getMindmap() {
+        return mindmap;
+    }
+
+    public void setMindmap(Mindmap mindmap) {
+        this.mindmap = mindmap;
+    }
+
+    @Override
+    public void onUserOpened(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public void onMindmapOpened(Mindmap mindmap) {
+        this.mindmap = mindmap;
+    }
+
+    @Override
+    public void onUserChanged(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public void onMindmapChanged(Mindmap mindmap) {
+        this.mindmap = mindmap;
+    }
+
+    @Override
+    public void onUserClosed() {
+        this.user = null;
+    }
+
+    @Override
+    public void onMindmapClosed() {
+        this.mindmap = null;
+    }
+
+    @Override
+    public void onException(Exception e) {
+        this.exception = e;
+    }
+}
