e4d9c5ce2089d62f36043abfc5d327d6b8365306
LootaBox <tupelibox@gmail.com>
2016-03-10 12:36:59 +0200

    Removed ability to set line color, because that was theme's job...

--- app/src/main/java/com/aivo/hyperion/aivo/models/Line.java
+++ app/src/main/java/com/aivo/hyperion/aivo/models/Line.java
@@ -15,7 +15,6 @@ public class Line {
     private MagnetGroup magnetGroup2;
     private ArrayList<PointF> points;
     private int type;
-    private int color;
     private int thickness;
 
     // The model mediator reference
@@ -37,28 +36,21 @@ public class Line {
     public MagnetGroup getMagnetGroup2() { return magnetGroup2; }
     public ArrayList<PointF> getPoints() { return points; }
     public int getType() { return type; }
-    public int getColor() { return color; }
     public int getThickness() { return thickness; }
 
     // DO NOT USE! Only for LineChangeData action!
-    public void setData(final int newType, final int newColor, final int newThickness) {
+    public void setData(final int newType, final int newThickness) {
         type = newType;
-        color = newColor;
         thickness = newThickness;
     }
 
     public void actionChangeType(final int newType) {
-        Action action = new LineChangeData(this, newType, color, thickness);
-        mediator.getMindmap().getActionHandler().executeAction(action);
-    }
-
-    public void actionChangeColor(final int newColor) {
-        Action action = new LineChangeData(this, type, newColor, thickness);
+        Action action = new LineChangeData(this, newType, thickness);
         mediator.getMindmap().getActionHandler().executeAction(action);
     }
 
     public void actionChangeThickness(final int newThickness) {
-        Action action = new LineChangeData(this, type, color, newThickness);
+        Action action = new LineChangeData(this, type, newThickness);
         mediator.getMindmap().getActionHandler().executeAction(action);
     }
 
--- app/src/main/java/com/aivo/hyperion/aivo/models/actions/LineChangeData.java
+++ app/src/main/java/com/aivo/hyperion/aivo/models/actions/LineChangeData.java
@@ -8,33 +8,29 @@ public class LineChangeData extends Action {
     private Line line;
     private int typeNew;
     private int typeOld;
-    private int colorNew;
-    private int colorOld;
     private int thicknessNew;
     private int thicknessOld;
 
-    public LineChangeData(Line line, final int typeNew, final int colorNew, final int thicknessNew) {
+    public LineChangeData(Line line, final int typeNew, final int thicknessNew) {
         this.line = line;
 
         this.typeNew = typeNew;
-        this.colorNew = colorNew;
         this.thicknessNew = thicknessNew;
 
         this.typeOld = line.getType();
-        this.colorOld = line.getColor();
         this.thicknessOld = line.getThickness();
     }
 
     @Override
     void execute() {
-        line.setData(typeNew, colorNew, thicknessNew);
+        line.setData(typeNew, thicknessNew);
         for (ModelListener listener : mediator.getListeners())
             listener.onLineChange(line);
     }
 
     @Override
     void undo() {
-        line.setData(typeOld, colorOld, thicknessOld);
+        line.setData(typeOld, thicknessOld);
             for (ModelListener listener : mediator.getListeners())
                 listener.onLineChange(line);
     }
